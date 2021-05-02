5a60c2a1fdeb2e932c85a075de82e56f370537f3
aking <alan.king@essentialskillz.com>
2021-05-02 08:31:19 +0100

    Layout, Pages and Routes

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-6">
+    <h1>About Vuetify Todo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat vitae cum eveniet nemo alias sunt placeat possimus consectetur rem doloribus, illum aspernatur quisquam consequatur eius ab non iure error et!</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat vitae cum eveniet nemo alias sunt placeat possimus consectetur rem doloribus, illum aspernatur quisquam consequatur eius ab non iure error et!</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Tenetur dolorem incidunt minima, voluptate ex reiciendis, assumenda id qui laudantium distinctio odit porro nesciunt soluta ipsa enim quia sequi est consectetur!</p>
   </div>
 </template>
